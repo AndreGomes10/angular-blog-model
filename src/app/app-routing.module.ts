6bfa8a18682870d6d786151cc2d47200ea141553
AndreGomes10 <andrezinholg10@gmail.com>
2023-10-10 11:17:44 -0300

    project create title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
     component: HomeComponent
   },
   {
-    path: 'content',
+    path: 'content/:id',
     component: ContentComponent
   }
 ];
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "Lamborghini Huracan",
+        "description": "Lançado em 2013 como o cupê de entrada da marca, o Lamborghini Huracan finalmente receberá um sucessor. Apesar de ainda manter em segredo todos os detalhes sobre o supercarro.",
+        "photoCover": "https://cdn.motor1.com/images/mgl/kNREB/s1/lamborghini-huracan-sto.jpg"
+    },
+    {
+        "id": "2",
+        "title": "Ferrari F8",
+        "description": "A Via Itália, importadora oficial da Ferrari no Brasil, confirmou o lançamento da F8 Tributo no país. O superesportivo chega para pôr um fim na 488 GTB/Spider/Pista e já está à venda no mercado.",
+        "photoCover": "https://midias.agazeta.com.br/2022/12/23/ferrari-f8-tributo-936571.jpg"
+    },
+    {
+        "id": "3",
+        "title": "Lamborghini Huracan",
+        "description": "Lançado em 2013 como o cupê de entrada da marca, o Lamborghini Huracan finalmente receberá um sucessor. Apesar de ainda manter em segredo todos os detalhes sobre o supercarro.",
+        "photoCover": "https://cdn.motor1.com/images/mgl/kNREB/s1/lamborghini-huracan-sto.jpg"
+    },
+    {
+        "id": "4",
+        "title": "Ferrari F8",
+        "description": "A Via Itália, importadora oficial da Ferrari no Brasil, confirmou o lançamento da F8 Tributo no país. O superesportivo chega para pôr um fim na 488 GTB/Spider/Pista e já está à venda no mercado.",
+        "photoCover": "https://midias.agazeta.com.br/2022/12/23/ferrari-f8-tributo-936571.jpg"
+    }
+]
