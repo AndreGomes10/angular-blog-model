9872bf677d5ea5dda61b80653a89e3b36e535694
AndreGomes10 <andrezinholg10@gmail.com>
2023-10-10 06:44:20 -0300

    project create pages and components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { EmailComponent } from './components/email/email.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
+import { ContentComponent } from './pages/content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuBarComponent,
+    MenuTitleComponent,
+    EmailComponent,
+    SmallCardComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule,
